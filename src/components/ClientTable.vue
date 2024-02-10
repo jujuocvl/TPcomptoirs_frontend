50d2a2946f8178dc12aed46b47e49e74070c5dd4
jujuocvl <julie.orcival@gmail.com>
2024-02-10 23:03:04 +0100

    10/02

---
+++ src/components/ClientTable.vue
@@ -0,0 +1,42 @@
+<template>
+    <main>
+        <div>
+            <h1>Liste des Clients</h1>
+            <DataTable :value="data.listeClients" paginator :rows="5" tableStyle="min-width: 50rem">
+                <Column field="code" header="Code" style="width: 25%"></Column>
+                <Column field="societe" header="Société" style="width: 25%"></Column>
+                <Column field="contact" header="Contact" style="width: 25%"></Column>
+                <Column field="adresse.ville" header="Ville" style="width: 25%"></Column>
+            </DataTable>
+        </div>
+    </main>
+</template>
+
+<script setup>
+import { reactive, onMounted } from "vue";
+import { doAjaxRequest } from "@/api";
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+
+let data = reactive({
+    listeClients: []
+});
+
+function showError(error) {
+    console.log("Erreur : status %d", error.status)
+    console.log(error.body);
+    alert(error.message);
+}
+
+function chargeClients() {
+    doAjaxRequest("/api/clients")
+        .then((json) => {
+            data.listeClients = json._embedded.clients;
+        })
+        .catch(showError);
+}
+
+//affichage composant = affichage de la liste
+onMounted(chargeClients);
+
+</script>
---
+++ src/views/ClientView.vue
@@ -0,0 +1,91 @@
+<template>
+<div>
+<table>
+  <caption>Liste des catégories</caption>
+  <tr>
+    <th>Code</th>
+    <th>Société</th>
+    <th>Contact</th>
+    <th>Ville</th>
+  </tr>
+  <!-- Si le tableau des catégories est vide -->
+  <tr v-if="data.listeCategories.length === 0">
+    <td colspan="4">Veuillez patienter, chargement des catégories...</td>
+  </tr>
+  <!-- Si le tableau des catégories n'est pas vide -->
+  <tr v-for="categorie in data.listeCategories" :key="categorie.code">
+    <td>{{ categorie.code }}</td>
+    <td>{{ categorie.societe }}</td>
+    <td>{{ categorie.contact }}</td>
+    <td>{{ categorie.adresse.ville }}</td>
+  </tr>
+  <tr>
+    <td><button type="number" @click="chargeCategories(data.listeLinks.first.href)">first</button></td>
+    <td><button v-if="typeof data.listeLinks.prev !== 'undefined'" type="number" @click="chargeCategories(data.listeLinks.prev.href)">prev</button></td>
+    <td><button v-if="typeof data.listeLinks.next !== 'undefined'" type="number" @click="chargeCategories(data.listeLinks.next.href)">next</button></td>
+    <td><button type="number" @click="chargeCategories(data.listeLinks.last.href)">last</button></td>
+
+  </tr>
+</table>
+</div>
+
+</template>
+
+<script setup>
+import {onMounted, reactive} from "vue";
+import {doAjaxRequest} from "@/api";
+
+//reinitialisation formulaire
+const categorieVide = {
+  libelle: "",
+  description: ""
+};
+
+const page = 0;
+
+let data = reactive({
+  // données formulaire
+  formulaireCategorie: {...categorieVide},
+  // liste des catégories sous forme de table
+  listeCategories: [],
+  listeLinks: []
+});
+
+function showError(error) {
+  console.log("Erreur : status %d", error.status)
+  console.log(error.body);
+  alert(error.message);
+}
+function chargeCategories(urlPage) {
+  // Appel à l'API pour liste des catégories
+  urlPage = urlPage.substring(urlPage.indexOf("/api"));
+  doAjaxRequest(urlPage)
+      .then((json) => {
+        data.listeCategories = json._embedded.clients;
+        data.listeLinks = json._links;
+        console.log(data.listeLinks);
+      })
+      .catch(showError);
+}
+
+onMounted(() => {
+  chargeCategories("/api/clients?&page=0&size=5");
+})
+
+</script>
+
+<style scoped>
+td,
+th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #232623;
+  color: rgb(255, 255, 255);
+}
+</style>
